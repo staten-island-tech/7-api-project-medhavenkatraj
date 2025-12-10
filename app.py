833613378c90b668412a8032240806d9fdec8d8d
medhavenkatraj <medhav@nycstudents.net>
2025-12-10 10:07:36 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,29 +5,26 @@ https://uselessfacts.jsph.pl/random.json
 
 import requests
 
-def getRandom_fact(fact):
+def getRandom_fact():
     response = requests.get(f"https://uselessfacts.jsph.pl/random.json")
     if response.status_code != 200:
         print("Error fetching data!")
         return None
     
     data = response.json()
-    return data 
+    return data
 
-fact = getRandom_fact()
-for key, value in fact.items():
-    print(key, "→", value)
-print(fact)
+random = getRandom_fact()
+for key, value in random.items():
+    print(f"{key.title()}: {value}")
 
+import tkinter as tk
 
+window = tk.Tk()
+window.title("Random Facts! ")
+window.geometry("400x250")
+window.resizable(False, False)
 
-
-
-
-
-""" {
-        "fact": data["fact"],
-        "height": data["height"],
-        "weight": data["weight"],
-        "types": [t["type"]["name"] for t in data["types"]]
-    } """
+prompt = tk.Label(window, text="Want to see a random fun fact? ",
+font=("Arial", 14))
+prompt.pack(pady=10)

d986ca5e96f02c55cdf6b33df8df25bf64ba7a17
medhavenkatraj <medhav@nycstudents.net>
2025-12-15 10:12:15 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,45 +8,35 @@ import requests
 import tkinter as tk
 
 
-def getRandom_fact():
-    response = requests.get(f"https://uselessfacts.jsph.pl/random.json")
-    return response.json()
+
 
 
 def check():
     if entry.get() == fact:
-        result_label.configure(text="Correct!")
+        result_label.configure(text = "correct!")
     else:
-        result_label.configure(text="Try again…")
+        result_label.configure(text = "try again…")
+
+def getRandom_fact():
+    response = requests.get(f"https://uselessfacts.jsph.pl/random.json")
+    x = response.json()
+    return  x["text"]
 
 
 fact = getRandom_fact()
-reversed_fact = fact[::-1]
+print(fact)
 
-
-
+reversed_fact = fact[:: -1]
 
 window = tk.Tk()
-window.title("Reverse the Fact")
-
-
+window.title("reverse the fact")
 tk.Label(window, text = "guess the reversed fun fact ").pack()
-
-
 tk.Label(window, text = reversed_fact, wraplength = 300).pack(pady = 10)
-
-
-entry = tk.Entry(window, width=40)
+entry = tk.Entry(window, width = 40)
 entry.pack()
-
-
-tk.Button(window, text = "Check", command=check).pack(pady = 10)
-
-
+tk.Button(window, text = "check", command = check).pack(pady = 10)
 result_label = tk.Label(window, text = "")
 result_label.pack()
-
-
 window.mainloop()
 
 
